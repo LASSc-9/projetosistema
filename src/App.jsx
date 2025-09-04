36fca670f19b7bc3673c1bc86ce2378a313b0921
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-04 08:29:56 -0300

    adicionando correçoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Nav from './components/Nav'
+import Error from './routes/Error'
+import Home from './routes/Home'
+import Cliente from './routes/Cliente'
+import Footer from './components/Footer'
 
 function App() {
+  
 
   return (
-    <>
-      
-    </>
+    //Habilita a navegação para rotas na aplicação
+    <BrowserRouter>
+    {/* Chamando o componete Nav */}
+    <Nav/>
+    <main>
+      {/* Gerencia a exibição dos componentes com base na URL */}
+      <Routes>
+        {/* Rota para chamar URL não encontradas */}
+        <Route path="*" element={<Error/>}/>
+
+        {/* Rota para chamar o Home */}
+        <Route path="/" element={<Home/>}/>
+
+        {/* Rota para chamar o cliente */}
+        <Route path="/cliente" element={<Cliente/>}/>
+
+
+      </Routes>
+    </main>
+    <Footer/>
+
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import {Link} from 'react-router-dom'
+import Logo from '../assets/imagem.png'
+
+const Nav = () => {
+  return (
+    <header className='bg-gray-800 text-white p-4 shadow-md'>
+      <nav className='container mx-auto flex justify-between items-center'>
+        <img src={Logo} alt="logo" className='h-10 w-auto'/>
+        <ul className='flex space-x-4'>
+          <li className='list-none'>
+            <Link to="/" className='hover:text-red-300 transition-colors duration-300'>Home</Link>
+          </li>
+          <li className='list-none'>
+            <Link to="/cliente" className='hover:text-red-300 transition-colors duration-300'>Cliente</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
