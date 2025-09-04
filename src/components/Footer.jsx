270456d3e803b81d70c81abf1bea30b95f48b621
Laboratório FIAP <lucaslugui1604@gmail.com>
2025-09-04 08:57:01 -0300

    estilizando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
-
+import { FaFacebook } from "react-icons/fa"
+import { FaInstagram } from "react-icons/fa"
 const Footer = () => {
   return (
-    <footer></footer>
+    <footer className="bg-gray-900 text-white p-6  flex flex-col items-center justify-center space-y-4">
+      <h3>&copy; 2025 - todos os direitos reservados</h3>
+      <div className="flex justify-around">
+        <a>
+          <FaFacebook/>
+        </a>
+        <a>
+          <FaInstagram/>
+        </a>
+      </div>
+    </footer>
   )
 }
 
